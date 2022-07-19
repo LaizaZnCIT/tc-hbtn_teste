afd051301237c0fe984816bc798ff00b2998fb72
LaizaZnCIT <laiza@ciandt.com>
2022-07-19 14:04:17 -0300

    JUnit parte 1 task 0

---
+++ 0x01/email/src/TestePessoa.java
@@ -0,0 +1,21 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class TestePessoa extends TestCase {
+
+    @Test
+    public void testar_email_com_arroba(){
+        assertTrue(Pessoa.emailValid("email_teste@dominio.com.br"));
+    }
+
+    @Test
+    public void testar_email_sem_arroba(){
+        assertFalse(Pessoa.emailValid("email_testedominio.com.br"));
+    }
+
+    @Test
+    public void testar_email_mais_50_caracteres(){
+        assertFalse(Pessoa.emailValid("email_teste_muito_longo_nao_deve_ser_valido@dominio.com.br"));
+    }
+
+}
